11935c2a479567fcd22eded474ac589fa8a2ad5f
george-kaf <106911993+george-kaf@users.noreply.github.com>
2023-02-08 15:07:37 +0000

    Update bikeztube_revisited.py

--- bikeztube_revisited.py
+++ bikeztube_revisited.py
@@ -22,10 +22,12 @@ root.title("Bikeztube")
 
 screen_width = root.winfo_screenwidth()
 screen_height = int(root.winfo_screenheight()/1.05)
-screen_resolution = str(screen_width)+'x'+str(screen_height)
-print(screen_resolution)
+# screen_resolution = str(screen_width)+'x'+str(screen_height)
+# print(screen_resolution)
 
-root.geometry(screen_resolution+'+-10+-1')
+# root.geometry(screen_resolution+'+-10+-1')
+
+root.attributes('-fullscreen')
 
 set_appearance_mode("dark")  # Modes: "System" (standard), "Dark", "Light"
 set_default_color_theme("green")  # Themes: "blue" (standard), "green", "dark-blue"
@@ -258,6 +260,285 @@ def update_bike():
     except:
         pass
 
+
+def get_history():
+    
+    his = CTkToplevel(root) 
+    his.state('zoomed') 
+    his.wm_transient(root)  
+    
+
+
+    def update_bike():
+
+        selected = my_hist_tree.focus()
+        my_hist_tree.item(selected, text='', values=(name_entry2.get(),phone_entry2.get(), bike_entry2.get(), id_entry2.get(), date_entry2.get(), work_entry2.get('1.0', END), total_price_entry2.get()))
+
+        conn = sqlite3.connect('bikes.db')
+        c = conn.cursor()
+        c.execute("""UPDATE history SET
+            name = :name,
+            phone = :phone,
+            bike = :bike,
+            date = :date,
+            work = :work,
+            total = :total
+            WHERE oid = :oid""",
+            {
+                    'name':name_entry2.get(),
+                    'phone':phone_entry2.get(),
+                    'bike':bike_entry2.get(),
+                    'oid':id_entry2.get(),
+                    'date':date_entry2.get(),
+                    'work':work_entry2.get('1.0', END),
+                    'total':total_price_entry2.get()
+
+            }
+        )
+
+        conn.commit()
+        conn.close()
+
+        my_hist_tree.delete(*my_hist_tree.get_children())
+        query_history()
+        clear_hist_entries()
+
+
+    def clear_hist_entries():
+        name_entry2.delete(0, END)
+        phone_entry2.delete(0, END)
+        bike_entry2.delete(0, END)
+        id_entry2.delete(0, END)
+        work_entry2.delete('1.0', END)
+        total_price_entry2.delete(0, END)
+
+    def delete_bike():
+        x = my_hist_tree.selection()[0]
+        my_hist_tree.delete(x)
+
+        conn = sqlite3.connect('bikes.db')
+        c = conn.cursor()
+        c.execute("DELETE from history WHERE oid=" + id_entry2.get())
+        conn.commit()
+        conn.close()
+        clear_hist_entries()
+
+        messagebox.showinfo("Deleted!", "Your Bike Service Has Been Deleted!")
+
+    def select_record_hist(e):
+        try:
+            name_entry2.delete(0, END)
+            phone_entry2.delete(0, END)
+            bike_entry2.delete(0, END)
+            id_entry2.delete(0, END)
+            date_entry2.delete(0, END)
+            work_entry2.delete('1.0', END)
+            total_price_entry2.delete(0, END)
+
+            selected = my_hist_tree.focus()
+            values = my_hist_tree.item(selected, "values")
+
+            name_entry2.insert(0, values[0])
+            phone_entry2.insert(0, values[1])
+            bike_entry2.insert(0, values[2])
+            id_entry2.insert(0, values[3])
+            date_entry2.insert(0, values[4])
+            work_entry2.insert('1.0', values[5])
+            total_price_entry2.insert(0, values[6])
+        except:
+            pass   
+
+
+    
+    def reset():
+        my_hist_tree.delete(*my_hist_tree.get_children())
+        global count
+        count = 0
+        conn = sqlite3.connect('bikes.db')
+        c = conn.cursor()
+        c.execute('SELECT rowid, * FROM history')
+
+        name_entry2.delete(0, END)
+        phone_entry2.delete(0, END)
+        bike_entry2.delete(0, END)
+        id_entry2.delete(0, END)
+        date_entry2.delete(0, END)
+        work_entry2.delete('1.0', END)
+        total_price_entry2.delete(0, END)
+
+        results = c.fetchall()
+
+        records = sorted(results, key=lambda tup: tup[0], reverse=True)
+
+        for record in records:
+            if count % 2 ==0:
+                my_hist_tree.insert(parent='', index='end', iid=count, text='', values=(str(record[1]), str(record[2]), str(record[3]), record[0], str(record[5]), str(record[6]), str(record[7])), tags=('evenrow',))
+            else:
+                my_hist_tree.insert(parent='', index='end', iid=count, text='', values=(str(record[1]), str(record[2]), str(record[3]), record[0], str(record[5]), str(record[6]), str(record[7])), tags=('oddrow',))
+            #increment counter
+            count +=1
+
+        conn.commit()
+        conn.close() 
+
+
+    def searcht():
+        query = searchbox_entry.get()
+        selections = []
+        for child in my_hist_tree.get_children():
+                selections.append(my_hist_tree.item(child)['values'])
+        my_hist_tree.delete(*my_hist_tree.get_children())
+
+        s_results = []
+        for i in selections:
+            if query.lower() in (str(i)).lower(): 
+                s_results.append(i)
+
+        for l in s_results:
+            global count
+            my_hist_tree.insert(parent='', index='end', iid=count, text='', values=(l[0], str(l[1]), l[2], l[3], l[4], l[5], l[6]))
+            count +=1
+   
+
+
+    def query_history():
+
+        global count
+        count = 0
+        conn = sqlite3.connect('bikes.db')
+        c = conn.cursor()
+        c.execute('SELECT rowid, * FROM history')
+
+        results = c.fetchall()
+        records = sorted(results, key=lambda tup: tup[0], reverse=True)
+
+        for record in records:
+            if count % 2 ==0:
+                my_hist_tree.insert(parent='', index='end', iid=count, text='', values=(str(record[1]), str(record[2]), str(record[3]), record[0], str(record[5]), str(record[6]), str(record[7])), tags=('evenrow',))
+            else:
+                my_hist_tree.insert(parent='', index='end', iid=count, text='', values=(str(record[1]), str(record[2]), str(record[3]), record[0], str(record[5]), str(record[6]), str(record[7])), tags=('oddrow',))
+            #increment counter
+            count +=1
+
+        conn.commit()
+        conn.close() 
+
+
+    hist_frame = CTkFrame(his)
+    hist_frame.pack(fill='both', expand=True, pady=button_width*2, padx=button_width*2)
+
+    my_hist_tree_frame = CTkFrame(hist_frame)
+    my_hist_tree_frame.pack(pady=(button_width*2, button_width*4), padx=button_width*2, side=LEFT, expand=True, fill='both')
+
+    buttons_hist_frame = CTkFrame(hist_frame)
+    buttons_hist_frame.pack(side=RIGHT, fill='y', expand=1, pady=(0, button_width))
+
+    my_hist_tree = ttk.Treeview(my_hist_tree_frame, selectmode='extended')
+    my_hist_tree.pack(fill='both', expand=True, pady=(button_width*2), padx=button_width*2)
+
+    my_hist_tree['columns'] = ('Name', 'Phone', 'Bike', 'ID', 'Date', 'Work', 'Total')
+
+    my_hist_tree.column('#0', width=0, stretch=NO)
+    my_hist_tree.column("Name", anchor=CENTER, width=120)
+    my_hist_tree.column("Phone", anchor=CENTER, width=120)
+    my_hist_tree.column("Bike", anchor=CENTER, width=200)
+    my_hist_tree.column("ID", anchor=CENTER, width=90)
+    my_hist_tree.column("Date", anchor=CENTER, width=110)
+    my_hist_tree.column("Work", anchor=CENTER, width=300)
+    my_hist_tree.column("Total", anchor=CENTER, width=80)
+
+    my_hist_tree.heading("#0", text="", anchor=CENTER)
+    my_hist_tree.heading("Name", text="Name", anchor=CENTER)
+    my_hist_tree.heading("Phone", text="Phone", anchor=CENTER)
+    my_hist_tree.heading("Bike", text="Bike", anchor=CENTER)
+    my_hist_tree.heading("ID", text="ID", anchor=CENTER)
+    my_hist_tree.heading("Date", text="Date", anchor=CENTER)
+    my_hist_tree.heading("Work", text="Work", anchor=CENTER)
+    my_hist_tree.heading("Total", text="Total", anchor=CENTER)
+
+    my_hist_tree.tag_configure('oddrow', background= '#319f6d', foreground='#ffffff')
+    my_hist_tree.tag_configure('evenrow',background='#288058', foreground='#ffffff')
+
+    entries_frame = CTkFrame(buttons_hist_frame)
+    entries_frame.pack(fill='y', expand=1)
+
+    search_frame = CTkFrame(entries_frame)
+    search_frame.pack(fill='x', expand=1, padx=button_width*2, pady=button_width)
+
+    searchbox_label = CTkLabel(search_frame, text='Search :  ', justify=CENTER)
+    searchbox_label.pack(side=LEFT, padx=(button_width*2,0), pady=(button_width*2))
+
+    searchbox_entry = CTkEntry(search_frame)
+    searchbox_entry.pack(side=RIGHT, padx=(0, button_width*2) , pady=button_width*2, fill='x', expand=1)
+
+    search_frame_buttons= CTkFrame(entries_frame)
+    search_frame_buttons.pack(fill='x', padx=(button_width*2), pady=(0, button_width)) 
+
+    reset_button = CTkButton(search_frame_buttons, text='Reset', command=reset)
+    reset_button.pack(side=LEFT, pady=button_width, padx=(button_width, int(button_width/2)), fill='x', expand=1)
+
+    search_button = CTkButton(search_frame_buttons, text='Go', command=searcht)
+    search_button.pack(side=RIGHT, pady=button_width, padx=(int(button_width/2), button_width), fill='x', expand=1)
+
+    entries_frame2 = CTkFrame(entries_frame)
+    entries_frame2.pack(fill='both', expand=1, padx=button_width*2)
+
+    date_entry2 = cal.DateEntry(entries_frame2, width=button_width*3, date_pattern='dd/MM/yyyy', font=('roboto', int(button_width*1.5)))
+    date_entry2.pack(fill='x', expand=1, padx=button_width*2)
+
+    id_label2 = CTkLabel(entries_frame2, text="ID (Always Leave Empty!)")
+    id_label2.pack()
+
+    id_entry2 = CTkEntry(entries_frame2)
+    id_entry2.pack()
+
+    name_label2 = CTkLabel(entries_frame2, text="Name")
+    name_label2.pack()
+
+    name_entry2 = CTkEntry(entries_frame2)
+    name_entry2.pack()
+
+    phone_label2 = CTkLabel(entries_frame2, text="Phone")
+    phone_label2.pack()
+
+    phone_entry2 = CTkEntry(entries_frame2)
+    phone_entry2.pack()
+
+    bike_label2 = CTkLabel(entries_frame2, text="Bike")
+    bike_label2.pack()
+
+    bike_entry2 = CTkEntry(entries_frame2)
+    bike_entry2.pack()
+
+    work_label2 = CTkLabel(entries_frame2, text="Work")
+    work_label2.pack()
+
+    work_entry2 = CTkTextbox(entries_frame2, height=3)
+    work_entry2.pack()
+
+    total_price_label2 = CTkLabel(entries_frame2, text="Total")
+    total_price_label2.pack()
+
+    total_price_entry2 = CTkEntry(entries_frame2)
+    total_price_entry2.pack()
+
+    buttons_hist = CTkFrame(entries_frame)
+    buttons_hist.pack(fill='x', expand=1, padx=button_width*2, pady=button_width*2)
+
+    del_button2 = CTkButton(buttons_hist, text="Update" , command=update_bike)
+    del_button2.pack(side=LEFT, pady=button_width, padx=(button_width, int(button_width/2)), fill='x', expand=1)
+
+    update_bike_button2 = CTkButton(buttons_hist, text="Delete", command=delete_bike)
+    update_bike_button2.pack(side=RIGHT, pady=button_width, padx=(int(button_width/2), button_width), fill='x', expand=1)
+
+    my_hist_tree.bind("<ButtonRelease-1>", select_record_hist)
+    query_history()
+
+
+    his.mainloop()
+
+
+
 def query_database():
 
     global count
@@ -365,9 +646,10 @@ def change_appearance_mode_event(new_appearance_mode: str):
         my_tree.tag_configure('oddrow', background= '#cccccc', foreground='#121212')
         my_tree.tag_configure('evenrow',background='#dddddd', foreground='#121212')
 
+
     if new_appearance_mode == "Dark":
         s.configure('Treeview.Heading', background="#181818", foreground='white', )
-        s.configure('Treeview', rowheight=int(button_width*3.5), fieldbackground="#363636")
+        s.configure('Treeview',  fieldbackground="#363636")
         s.map('Treeview', background=[('selected', '#319f6d')])
         my_tree.tag_configure('oddrow', background= '#363636', foreground='white')
         my_tree.tag_configure('evenrow',background='#303030', foreground='white')
@@ -411,6 +693,33 @@ def fixed_bike():
 
         messagebox.showinfo("Done!", "Service Record Saved !")
 
+
+def select_record(e):
+
+    global service_text
+
+
+    try:
+
+        curItem = my_tree.focus()
+        item_data = (my_tree.item(curItem))
+        item_values = item_data['values']
+        id_from_selection = str(item_values[4])
+
+        conn = sqlite3.connect('bikes.db')
+        c = conn.cursor()
+        c.execute('SELECT work FROM bikes WHERE rowid=' +id_from_selection )
+        records = c.fetchall()
+        records_list = records[0]
+        service_text.set('Service Details :  '+records_list[0])
+        conn.commit()
+        conn.close()
+
+       
+
+    except:
+        pass  
+
 menu_width = (screen_width/9)
 button_width = int(menu_width/14)
 
@@ -424,23 +733,20 @@ menu_frame.propagate(0)
 butt_padx = int(button_width)
 butt_pady = int(button_width)
 
-title_label = CTkLabel(menu_frame, text="Workshop Manager", font=('roboto', button_width*1.5))
+title_label = CTkLabel(menu_frame, text="Workshop Helper", font=('roboto', button_width*1.5))
 title_label.pack(pady=(int(button_width*3),int(button_width*1.5)),  padx=button_width*2 ,fill='x')
 
-butt1 = CTkButton(menu_frame, text='+ Add', command=add_bike, font=('roboto', int(button_width*1.1)))
+butt1 = CTkButton(menu_frame, text='+ Add', command=add_bike, font=('roboto', int(button_width*1.4), 'bold'))
 butt1.pack(ipady=int(button_width/2), pady=(int(button_width*2), 0), padx=button_width*2 ,fill='x')
 
-butt2 = CTkButton(menu_frame, text='Update', command=update_bike, font=('roboto',  int(button_width*1.1)))
+butt2 = CTkButton(menu_frame, text='Update', command=update_bike, font=('roboto',  int(button_width*1.4), 'bold'))
 butt2.pack(pady=button_width, ipady=int(button_width/2), padx=button_width*2 ,fill='x')
 
-butt3 = CTkButton(menu_frame, text='Ready', command=fixed, font=('roboto',  int(button_width*1.1)))
+butt3 = CTkButton(menu_frame, text='Ready', command=fixed, font=('roboto',  int(button_width*1.4), 'bold'))
 butt3.pack(ipady=int(button_width/2), padx=button_width*2 ,fill='x')
 
-butt4 = CTkButton(menu_frame, text='Collected', command=fixed_bike, font=('roboto',  int(button_width*1.1)))
+butt4 = CTkButton(menu_frame, text='Collected', command=fixed_bike, font=('roboto',  int(button_width*1.4), 'bold'))
 butt4.pack(pady=button_width, ipady=int(button_width/2), padx=button_width*2 ,fill='x')
-
-butt5 = CTkButton(menu_frame, text='Delete', command=delete_bike, font=('roboto',  int(button_width*1.1)))
-butt5.pack( ipady=int(button_width/2), padx=button_width*2 ,fill='x')
 
 appearance_mode_optionemenu = CTkOptionMenu(menu_frame, values=["Dark", "Light"], command=change_appearance_mode_event)
 appearance_mode_optionemenu.pack(side=BOTTOM, pady=(0,40))
@@ -448,8 +754,11 @@ appearance_mode_optionemenu.pack(side=BOTTOM, pady=(0,40))
 appearance_mode_label = CTkLabel(menu_frame, text="Appearance Mode:", anchor="w")
 appearance_mode_label.pack(side=BOTTOM)
 
+butt5 = CTkButton(menu_frame, text='Delete', command=delete_bike, font=('roboto',  int(button_width*1.4), 'bold'))
+butt5.pack(side=BOTTOM, ipady=int(button_width/2), pady=(0,button_width*2), padx=button_width*2 ,fill='x')
+
 content_frame = CTkFrame(main_frame)
-content_frame.pack(side=RIGHT, fill=BOTH, expand=1, pady=button_width*3, padx=button_width*3)
+content_frame.pack(side=RIGHT, fill=BOTH, expand=1, pady=(button_width*3, 0), padx=button_width*3)
 
 tree_and_bar_frame = CTkFrame(content_frame)
 tree_and_bar_frame.pack( expand=True, fill='both')
@@ -463,7 +772,7 @@ my_tree.pack(fill='both', expand=True)
 bar_and_label_frame = CTkFrame(tree_and_bar_frame)
 bar_and_label_frame.pack( fill='x', anchor=N, pady=(button_width*1.5, 0))
 
-bar_label = CTkLabel(bar_and_label_frame, text='Service capacity : ', font=('roboto', button_width, 'bold'))
+bar_label = CTkLabel(bar_and_label_frame, text='Shop capacity : ', font=('roboto', button_width, 'bold'))
 bar_label.pack(side=LEFT, pady=(button_width), padx=(button_width,0 ), anchor=N)
 
 progressbar = CTkProgressBar(master=bar_and_label_frame)
@@ -496,7 +805,7 @@ s = ttk.Style()
 s.theme_use('classic')
 
 s.configure('Treeview.Heading', background="#181818", foreground='white', font=('roboto', int(button_width*1.3), 'bold' ), height=14, borderwidth=0 )
-s.configure('Treeview', rowheight=int(button_width*3.5), font=('roboto' ,button_width ), fieldbackground="#363636", bordercolor='#262626')
+s.configure('Treeview', rowheight=int(button_width*6), font=('roboto' ,button_width*2 ), fieldbackground="#363636", bordercolor='#262626')
 s.map('Treeview', background=[('selected', '#319f6d')])
 
 more_butt_frame = CTkFrame(content_frame)
@@ -505,8 +814,10 @@ more_butt_frame.pack(fill='x',  side=BOTTOM)
 details_and_label_frame = CTkFrame(more_butt_frame)
 details_and_label_frame.pack(anchor=W,side=LEFT, fill='x', expand=1, pady=(button_width*1.5), padx=(0, button_width*1.5))
 
-service_details_label = CTkLabel(details_and_label_frame, height=button_width*6.3, wraplength=button_width*50, justify=LEFT, text='Commue, Front e PFront Geear Wire', font=('roboto', button_width))
-service_details_label.pack(anchor=W, pady=button_width, padx= button_width*1.5)
+service_text = StringVar()
+service_text.set('Click A Customer To Show Its Details')
+service_details_label = CTkLabel(details_and_label_frame, height=button_width*6.3, wraplength=button_width*50, justify=LEFT, textvariable=service_text , font=('roboto', button_width*1.5))
+service_details_label.pack(anchor=NW, pady=button_width, padx= button_width*1.5)
 
 stat_frame = CTkFrame(more_butt_frame)
 stat_frame.pack(side=RIGHT, pady=button_width*1.5, anchor=N)
@@ -514,21 +825,23 @@ stat_frame.pack(side=RIGHT, pady=button_width*1.5, anchor=N)
 checkbuttons_frame = CTkFrame(stat_frame)
 checkbuttons_frame.pack(pady=button_width, padx=(button_width*1.5))
 
-checkbox_1 = CTkCheckBox(master=checkbuttons_frame, text='Basic Service')
+checkbox_1 = CTkCheckBox(master=checkbuttons_frame, text='Basic Service', font=('roboto', button_width))
 checkbox_1.pack(pady=button_width, padx=(button_width*2, 0), side=LEFT)
 
-checkbox_2 = CTkCheckBox(master=checkbuttons_frame, text='Commuter Service')
+checkbox_2 = CTkCheckBox(master=checkbuttons_frame, text='Commuter Service', font=('roboto', button_width))
 checkbox_2.pack(pady=button_width, padx=(button_width, 0), side=LEFT)
 
-checkbox_3 = CTkCheckBox(master=checkbuttons_frame, text='Full Service', )
+checkbox_3 = CTkCheckBox(master=checkbuttons_frame, text='Full Service', font=('roboto', button_width))
 checkbox_3.pack(pady=button_width, padx=(button_width), side=LEFT)
 
-graph_butt = CTkButton(stat_frame, text='Statistics', font=('roboto',  int(button_width*1.1)))
+graph_butt = CTkButton(stat_frame, text='Statistics', font=('roboto',  int(button_width*1.1), 'bold'))
 graph_butt.pack(pady=(0, button_width), padx=( button_width*1.5), side=LEFT, fill='x', expand=1)
 
-hist_butt = CTkButton(stat_frame, text='History', font=('roboto',  int(button_width*1.1)))
+hist_butt = CTkButton(stat_frame, text='History', command=get_history, font=('roboto',  int(button_width*1.1), 'bold'))
 hist_butt.pack(pady=(0, button_width,), padx=(0, button_width*1.5), side=RIGHT, fill='x', expand=1)
 
 query_database()
+
+my_tree.bind("<ButtonRelease-1>", select_record)
 
 root.mainloop()
